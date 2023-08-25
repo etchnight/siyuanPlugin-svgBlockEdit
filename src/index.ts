6dc7d5a7a847735f9d081641d7b3276a23c4208d
dualwind <dualwind@qq.com>
2023-08-25 23:43:34 +0800

    增加读取写入svghtml功能

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
-import { Plugin, Dialog, getFrontend, getBackend } from "siyuan";
+import { Plugin, Dialog, openTab } from "siyuan";
 //import svgEdit from "@/libs/svg-edit.svelte";
+import SavePanel from "@/save.svelte";
+
 export default class siyuanPluginSvgBlockEdit extends Plugin {
   private blockIconEventBindThis = this.blockIconEvent.bind(this);
+
   //private blockId: BlockId;
   async onload() {
     console.log(this.i18n.helloPlugin);
@@ -9,7 +12,7 @@ export default class siyuanPluginSvgBlockEdit extends Plugin {
 
   onLayoutReady() {
     this.eventBus.on("click-blockicon", this.blockIconEventBindThis);
-    console.log(`frontend: ${getFrontend()}; backend: ${getBackend()}`);
+    //console.log(`frontend: ${getFrontend()}; backend: ${getBackend()}`);
   }
 
   onunload() {
@@ -25,6 +28,16 @@ export default class siyuanPluginSvgBlockEdit extends Plugin {
     if (ele.getAttribute("data-type") !== "NodeHTMLBlock") {
       return;
     }
+    const protyleELe = ele.querySelector("protyle-html");
+    const html = protyleELe.getAttribute("data-content");
+    let tempEle = document.createElement("div");
+    tempEle.innerHTML = html;
+    const svgEle = tempEle.querySelector("svg");
+    //console.log(html);
+    if (!svgEle && html) {
+      return;
+    }
+
     const blockId = ele.getAttribute("data-node-id");
 
     const loadEditor = this.loadEditor.bind(this);
@@ -37,29 +50,84 @@ export default class siyuanPluginSvgBlockEdit extends Plugin {
   }
   private loadEditor(blockId: BlockId) {
     //console.log(html);
-    const height = 540;
-    const width = 700;
-    const content = `<div class="b3-dialog__content" id="plugin-svgblockedit">
-                        <div class="protyle-wysiwyg protyle-wysiwyg--attr"  width="100%">
-                            <iframe id="siyuanPlugin-svgBlockEdit" width="100%" style="height: ${
-                              height - 50
-                            }px;"
-                                src="./plugins/siyuanPlugin-svgBlockEdit/libs/editor/index.html"
-                                targetId='${blockId}'>
-                            </iframe>
-                        </div>
-                    </div>`;
-    let dialog = new Dialog({
-      title: "svg编辑器",
-      transparent: false,
-      width: width + "px",
-      content: content,
-      height: height + "px",
+    //const height = 540;
+    //const width = 700;
+    const content = `<iframe id="siyuanPlugin-svgBlockEdit" src="./plugins/siyuanPlugin-svgBlockEdit/libs/editor/index.html"
+                        targetId='${blockId}'>
+                    </iframe>`;
+    const resizeObserver = new ResizeObserver((entries) => {
+      for (let entry of entries) {
+        //console.log(entry);
+        const height = entry.contentRect.height;
+        const width = entry.contentRect.width;
+        const iframe = document.getElementById("siyuanPlugin-svgBlockEdit");
+        try {
+          iframe.style.height = `${height * 0.9}px`;
+          iframe.style.width = `${width}px`;
+        } catch (e) {
+          return;
+        }
+      }
     });
+    const tab = openTab({
+      app: this.app,
+      custom: {
+        icon: "iconFace",
+        title: "svg编辑器",
+        data: {
+          text: "",
+        },
+        fn: this.addTab({
+          type: "custom_tab",
+          init() {
+            const thisElement = this.element as HTMLElement;
+            thisElement.innerHTML = content;
+            resizeObserver.observe(thisElement); //?是否有副作用,resize
+            /*function resize() {
+              console.log("resize");
+              const width = window.getComputedStyle(thisElement).width;
+              const height = window.getComputedStyle(thisElement).height;
+              const iframe = thisElement.querySelector("iframe");
+              iframe.style.height = `${parseInt(height) * 0.9}px`;
+              iframe.style.width = width;
+            }*/
+          },
+          beforeDestroy() {
+            let dialog = new Dialog({
+              title: "svg编辑器",
+              transparent: false,
+              width: "420px",
+              content: `<div id="savePanel" class="b3-dialog__content"></div>`,
+              height: "200px",
+            });
 
-    //const svgEditor = new Editor(document.getElementById("plugin-svgblockedit-container"));
-    //console.log(svgEditor)
-    //svgEditor.init();
-    //svgEditor.configObj.curConfig.imgPath="./plugins/siyuanPlugin-svgBlockEdit/libs/editor/images"
+            const iframe = this.element.querySelector(
+              "iframe"
+            ) as HTMLIFrameElement;
+            try {
+              const svgcontent =
+                iframe.contentDocument.getElementById("svgcontent");
+              //console.log(iframe);
+              new SavePanel({
+                target: dialog.element.querySelector("#savePanel"),
+                props: {
+                  blockId: blockId,
+                  svgHtml: svgcontent.outerHTML,
+                  dialog: dialog,
+                },
+              });
+            } catch (e) {
+              return;
+            }
+
+            //console.log("before destroy tab:");
+          },
+          destroy() {
+            resizeObserver.disconnect();
+            //console.log("destroy tab:");
+          },
+        }),
+      },
+    });
   }
 }
---
+++ src/save.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import { updateBlock } from "../../siyuanPlugin-common/siyuan-api";
+  export let blockId: string;
+  export let svgHtml: string;
+  export let dialog;
+  //console.log(blockId);
+  //console.log(svgHtml);
+  function saveSVG() {
+    updateBlock("markdown", `<div>${svgHtml}</div>`, blockId);
+    closeDialog();
+  }
+  function closeDialog() {
+    dialog.destroy();
+  }
+</script>
+
+<div class="b3-dialog__content">
+  <div class="fn__flex-1">是否保存？</div>
+  <div class="b3-dialog__action">
+    <button class="b3-button b3-button--cancel" on:click={closeDialog}
+      >取消</button
+    >
+    <div class="fn__space" />
+    <button class="b3-button b3-button--text" on:click={saveSVG}>保存</button>
+  </div>
+  <!--
+  <label class="fn__flex b3-label config__item">
+    <div class="fn__flex-1" />
+    <span class="fn__space" />
+    <button
+      class="b3-button b3-button--outline fn__flex-center fn__size100"
+      on:click={saveSVG}>保存</button
+    ></label
+  >
+  -->
+</div>
